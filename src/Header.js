9258243fa6d45283dccc1210b0277fcdf3e35ca0
shivasnr <shivasnr41@gmail.com>
2022-08-30 14:43:56 +0545

    organizing components in react

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header(){
+    return (
+      <header>
+        <nav className="nav">
+          <img src="./logo192.png" className="nav-logo"/>
+          <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Content</li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
+
