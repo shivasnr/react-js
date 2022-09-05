0c9e741409f2f1baedfcb471ef2ef7e981526d5c
shivasnr <shivasnr41@gmail.com>
2022-09-05 16:37:05 +0545

    destructuring the props in react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,30 @@
 import React from "react";
 
-export default function Contact(props) {
+export default function Contact({img, name, phone, email}) {
     return (
         <div className="contact-card">
-            <img src={props.img} />
-            <h3>{props.name}</h3>
+            <img src={img} />
+            <h3>{name}</h3>
             <div className="info-group">
                 <img src="./images/phone-icon.png" />
-                <p>{props.phone}</p>
+                <p>{phone}</p>
             </div>
             <div className="info-group">
                 <img src="./images/mail-icon.png" />
-                <p>{props.email}</p>
+                <p>{email}</p>
             </div>
         </div>
     )
 }
+
+// Object Destructuring
+/* const person = {
+    img: "./images/mr-whiskerson.png",
+    name: "Mr. David",
+    phone: "(800) 555-1234",
+    email: "mr.whiskaz@catnap.meow"
+}
+
+const {img, name} = person
+console.log(name)
+*/
